5a469dca07dd70deda58c0f7114049a16d98b193
Eliran Eliassy <eliran.eliassy@gmail.com>
2023-03-29 20:12:04 +0300

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { Component, signal, computed, effect, OnInit } from '@angular/core';
+import { fromObservable } from 'src/from-observable';
+import { fromSignal } from 'src/from-signal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'signals-playground';
+export class AppComponent implements OnInit {
+
+  state = signal({
+    counter: 0
+  });
+
+  state$ = fromSignal(this.state)
+
+  doubleCounter = computed(() => this.state().counter * 2)
+
+  timer$ = interval(1000);
+
+  timer = fromObservable(this.timer$, 0);
+
+  ngOnInit(): void {
+    effect(() => console.log('counter is', this.state().counter))
+  }
+
+
+  plus() {
+    // this.counter.set(100);
+
+    // this.state.update((state) => ({ ...state, counter: state.counter + 1 }))
+
+    this.state.mutate((state) => state.counter = state.counter + 1)
+  }
+
+
 }
